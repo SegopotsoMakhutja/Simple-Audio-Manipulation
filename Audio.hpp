7ec52a6366bd1fef689877c2213e44556840268c
Segopotso <leketi.s@gmail.com>
2019-05-10 05:08:40 +0200

    audio header file created, added includes.

---
+++ Audio.hpp
@@ -0,0 +1,16 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <numeric>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <cstdint>
+#include <utility>
+#include <type_traits>
+
+#endif
